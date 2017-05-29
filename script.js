26b98bacad73ab3e8fa59fa5b0b8a856406400b9
ealmutair <essam.ali.almutair@gmail.com>
2017-05-29 10:19:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,5 +6,7 @@ function load() {
 	document.getElementById("output4").innerHTML=(mydata[1].specialty),
 	document.getElementById("output5").innerHTML=(mydata[1].practice_name);
 }
+function sort(){
 
+}
 data = '[{"last_name" : "Ashwin", "first_name" : "20","email_address":"mharris@updox.com","specialty": "Pediatrics", "practice_name": "Harris Pediatrics"},{"last_name" : "Abhinandan", "first_name" : "20","email_address": "bwijoyo@updox.com","specialty": "Podiatry", "practice_name": "Wijoyo Podiatry"},{"last_name": "Rose", "first_name": "Nate", "email_address": "nrose@updox.com", "specialty": "Surgery", "practice_name": "Rose Cutters"},{"last_name": "Carlson", "first_name": "Mike", "email_address": "mcarlson@updox.com", "specialty": "Orthopedics", "practice_name": "Carlson Orthopedics"},{"last_name": "Witting", "first_name": "Mike", "email_address": "mwitting@updox.com", "specialty": "Pediatrics", "practice_name": "Witting’s Well Kids Pediatrics"}, {"last_name": "Juday", "first_name": "Tobin", "email_address": "tjuday@updox.com", "specialty": "General Medicine", "practice_name": "Juday Family Practice"}]';
